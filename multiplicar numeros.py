dd28c352745c3368a84b989532e4f513382db0d3
Adriancr755 <cristiandelacruz564@gmail.com>
2019-02-05 14:54:27 -0600

    multiplicar, sumar, dividir y restar numeros

---
+++ multiplicar numeros.py
@@ -0,0 +1,31 @@
+##crear un programa que se encojan si 1,sumar 2 numeros 2,restar 3 numeros
+##3,multiplicar 4 numeros, si el usurario escoje una opcion invalidad
+##hacersolo saber
+
+n 1 = int(input("num1: ")
+n 2 = int(input("num2: ")
+print("el resultado es: " str(n1+n2))
+
+
+
+r1 = int(input("#uno"))
+r2 = int(input("#dos"))
+r3 = int(input("#tres"))
+total= r1-r2-r3
+pritn ("el toal es: ",total)
+
+
+m1 = int(input("#1"))
+m2 = int(input("#2"))
+m3 = int(input("#3"))
+m4 = int(input("#4"))
+totalM = m1*m2*m3*m4
+print("el total de la multiplicacion es: ",totalM))
+
+
+
+
+n1 = int(input("n1: ")
+n2 = int(input("n2: ")
+print("el total es: ",total)
+         
